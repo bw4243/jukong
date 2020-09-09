5da06c4bb4f5942b77316de808090650ca358cb8
bayer <bw4243@qq.com>
2020-09-09 17:07:14 +0800

    init

--- dpi/controllers/MonitorController.php
+++ dpi/controllers/MonitorController.php
@@ -14,6 +14,7 @@ class MonitorController extends Controller
     public function actionIndex() {
         $json = file_get_contents('php://input');
         $json= str_replace("id", '"id"', $json);
+        $json="{".$json."}";
         $post= (new \yii\helpers\BaseJson)->decode($json);
         //var_dump($post);die;
         //echo \Yii::getAlias('@dpi').DIRECTORY_SEPARATOR.'info'.DIRECTORY_SEPARATOR;die;
